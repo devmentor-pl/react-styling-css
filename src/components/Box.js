385390fbae47a791bd156b76960451f372ac68e6
Mateusz Bogolubow <mateusz@bogolubow.com>
2020-03-29 08:51:12 +0200

    Init

---
+++ src/components/Box.js
@@ -0,0 +1,14 @@
+// ./src/components/Box.js
+import React from 'react';
+import Row from './Row';
+
+const Box = () => {
+    return (
+        <>
+            <Row type="dark" />
+            <Row space="medium"/>
+        </>
+    );
+}
+
+export default Box;
